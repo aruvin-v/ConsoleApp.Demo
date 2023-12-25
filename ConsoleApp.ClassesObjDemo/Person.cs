b2d95f5c76ec38470a6c85243fb6ea42e9190102
aruvin-v <aruvin.vasudevan@gmail.com>
2023-12-25 11:35:54 +0530

    class addition

---
+++ ConsoleApp.ClassesObjDemo/Person.cs
@@ -0,0 +1,29 @@
+﻿//Class - blueprint
+//Single Responsibility Priciple
+public class Person
+{
+    //public Properties (like nouns)
+    public string FirstName { get; set; } //Get and Set are Accessors
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    //Private fields
+    private double _salary { get; set; }
+
+    public void setSalary(double value)
+    {
+        _salary = value;
+    }
+    public double getSalary()
+    {
+        return _salary;
+    }
+    public string getFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+    public int getBirthYear()
+    {
+        int birthYear = DateTime.Now.Year - Age;
+        return birthYear;
+    }
+}
---
+++ ConsoleApp.ClassesObjDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Sample");
+
+//Creating an oblect
+Person person = new Person();
+
+Console.Write("Enter First Name: ");
+person.FirstName = Console.ReadLine();
+
+Console.Write("Enter Last Name: ");
+person.LastName = Console.ReadLine();
+
+Console.Write("Enter Age: ");
+person.Age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Salary: ");
+double salary = Convert.ToInt32(Console.ReadLine());
+person.setSalary(salary);
+
+//Console.WriteLine($"First name is {person.FirstName}");
+//Console.WriteLine($"Last name is {person.LastName}");
+Console.WriteLine($"Full name is {person.getFullName()}");
+Console.WriteLine($"Born year is {person.getBirthYear()}");
+Console.WriteLine($"The salary is {person.getSalary()}");
