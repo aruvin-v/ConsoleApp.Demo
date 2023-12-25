6c3b01106430c6623df753e7f7164dc7b9cd6d4a
aruvin-v <aruvin.vasudevan@gmail.com>
2023-12-25 10:55:15 +0530

    Added Lists

---
+++ ConsoleApp.ListCollectionsDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿//Declare
+List<string> names = new List<string>();
+string name = "";
+
+//Add values to the list
+while (name != "-1")
+{
+    Console.Write("Enter name: ");
+    name = Console.ReadLine();
+    if(!string.IsNullOrEmpty(name) && !name.Equals("-1"))
+    {
+        names.Add(name);
+        Console.Write($"{name} was added successfully\n");
+    }
+}
+
+//Print values in the list
+foreach(var item in names)
+{
+    Console.WriteLine(item);
+}
