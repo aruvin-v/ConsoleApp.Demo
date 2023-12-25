09869999a26e84aaa2ca5e788824f87530c6ea9a
aruvin-v <aruvin.vasudevan@gmail.com>
2023-12-25 10:28:12 +0530

    Added string manipulation

---
+++ ConsoleApp.StringManipulationDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿string firstName = "Samuel";
+string lastName = "Maxson";
+
+//Concatenation
+string fullName = firstName +" " + lastName;
+
+//String length
+int length = firstName.Length;
+Console.WriteLine($"Your first name contains {length} characters");
+
+//Replace string
+string newName = firstName.Replace('S', 'D');
+Console.WriteLine($"The new name is {newName}");
+
+//Split string
+string[] splitName = fullName.Split(' ');
+for(int i = 0; i < splitName.Length; i++)
+{
+    Console.WriteLine(splitName[i]);
+}
+
+//Compare strings
+string sampleStr = null;
+if (string.IsNullOrEmpty(sampleStr))
+{
+    Console.WriteLine("String is null");
+}
+if(firstName != lastName)
+{
+    Console.WriteLine("Names are distinct");
+}
+
